9dbb168829ddaeca2b08d4a5ff41d2971d3c61b6
Ogundana Ayomide <89689966+ayomide2021-rgb@users.noreply.github.com>
2023-09-14 06:23:30 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+// Show the back-to-top button when scrolling down
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("topButton").style.display = "block";
+    } else {
+        document.getElementById("topButton").style.display = "none";
+    }
+}
+
+// Scroll to the top when the button is clicked
+function scrollToTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+}
+
+
+/* Set the width of the sidebar to 250px (show it) */
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "80%";
+  }
+  
+  /* Set the width of the sidebar to 0 (hide it) */
+  function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+  }
